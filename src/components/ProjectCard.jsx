bd2c24066f9bf824285f5a48a209b4e09a4760ad
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-15 11:13:52 +0530

    chore: fixed spacing in skills.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,20 +1,20 @@
-const ProjectCard = ({ image, title, description, tech, github, demo }) => {
+const ProjectCard = ({ image, title, description, tech, github, link }) => {
   return (
-    <div className="bg-brand-light rounded-xl shadow-md overflow-hidden">
+    <div className="bg-brand-light rounded-xl shadow-md overflow-hidden flex flex-col h-full">
       <img src={image} alt={title} className="w-full h-48 object-cover" />
-      <div className="p-6 spacce-y-3">
+      <div className="p-6 space-y-3 flex flex-col grow">
         <h3 className="text-xl font-semibold">{title}</h3>
-        <p className="text-sm text-gray-600">
+        <p className="text-sm text-gray-600 grow">
           {description}
         </p>
-        <div>
+        <div className="flex flex-wrap gap-2">
             {tech.map((item, index) => (
                 <span key={index} className="bg-brand-cream text-brand-dark px-3 py-1 rounded-full text-xs font-medium">{item}</span>
             ))}
         </div>
-        <div className="flex gap-4 pt-2">
-            <a href={github} target="_blank" className="px-4 py-2 text-sm bg-brand-dark text-white rounded-lg hover:opacity-90 transition">Github</a>
-            <a href={demo} target="_blank" className="px-4 py-2 text-sm border border-brand-dark rounded-lg hover:bg-brand-dark hover:text-white transition">Live Demo</a>
+        <div className="flex gap-4 pt-2 mt-auto">
+            <a href={github} target="_blank" className="px-4 py-2 text-sm bg-brand-dark text-white rounded-lg hover:opacity-90 transition cursor-pointer">Github</a>
+            <a href={link} target="_blank" className="px-4 py-2 text-sm border border-brand-dark rounded-lg hover:bg-brand-dark hover:text-white transition cursor-pointer">Live Demo</a>
         </div>
       </div>
     </div>
