a11f35366ba2ae20782a319d840b07e214f5bb1d
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-15 11:11:28 +0530

    feat(github-link): Added Github Links for Project Cards.

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,24 +1,29 @@
+import portfolio from "../assets/projects/portfolio.png";
+
 export const projects = [
   {
-    image: "/project1.png",
+    image: portfolio,
     title: "Portfolio Website",
     description: "A personal portfolio website showcasing my projects.",
     tech: ["React", "TailwindCSS"],
-    link: "#",
+    link: "https://harivishvanath-portfolio.vercel.app/",
+    github: "https://github.com/harivishvanath7/Portfolio"
   },
   {
     image: "/project2.png",
-    title: "Task Manager",
-    description: "A task management app to track daily work.",
-    tech: ["React", "Node.js", "MongoDB"],
+    title: "DevConnect",
+    description: "A Community Made for and by the Developers.",
+    tech: ["React", "Node.js", "Express", "MongoDB", "JWT", "REST APIs"],
     link: "#",
+    github: "https://github.com/harivishvanath7/Dev_Connect"
   },
   {
     image: "/project3.png",
-    title: "Weather App",
-    description: "Shows weather using an external API.",
-    tech: ["JavaScript", "API", "CSS"],
+    title: "AI Startup Valuator",
+    description: "A Platform to valuate your Startup, helps in optimizing performances and provides metrics accordingly.",
+    tech: ["React.js", "Node.js", "Express.js", "MongoDB", "Groq API", "LLaMA 3", "REST APIs"],
     link: "#",
+    github: "https://github.com/harivishvanath7/AI_Startup_Valuator"
   },
 ];
 
